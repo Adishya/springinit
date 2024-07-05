506a81d00a18a3eb4e0e4c473e6b3a327773d5fa
adishya <adishyauchil0779@gmail.com>
2024-07-05 20:17:25 +0530

    data saved

--- src/main/java/com/user/register/homeserviceimpl/RegisterServiceImpl.java
+++ src/main/java/com/user/register/homeserviceimpl/RegisterServiceImpl.java
@@ -26,42 +26,6 @@ public class RegisterServiceImpl implements RegisterService {
         return registerRepo.save(register);
     }
 
-    public void generateExcelFile(HttpServletResponse response) throws IOException {
-        List<RegisterEntity> registerList = registerRepo.findAll();
-        System.out.println("Number of entities retrieved: " + registerList.size());
-
-        XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet = workbook.createSheet("User Info");
-
-        // Creating header row
-        XSSFRow headerRow = sheet.createRow(0);
-        headerRow.createCell(0).setCellValue("ID");
-        headerRow.createCell(1).setCellValue("Name");
-        headerRow.createCell(2).setCellValue("Email");
-        headerRow.createCell(3).setCellValue("Phone Number");
-
-        // Writing data rows
-        int dataRowIndex = 1;
-        for (RegisterEntity entity : registerList) {
-            XSSFRow dataRow = sheet.createRow(dataRowIndex);
-            dataRow.createCell(0).setCellValue(entity.getId());
-            dataRow.createCell(1).setCellValue(entity.getName());
-            dataRow.createCell(2).setCellValue(entity.getEmail());
-            dataRow.createCell(3).setCellValue(entity.getPhonenumber());
-            dataRowIndex++;
-        }
-
-   
-        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        response.setHeader("Content-Disposition", "attachment; filename=customerdetail.xlsx");
-
-
-        ServletOutputStream ops = response.getOutputStream();
-        workbook.write(ops);
-
-        // Closing resources
-        workbook.close();
-        ops.close();
-    }
+    
 }
 
